9f703dd699ff56a30046d67ecab859c5e2c59d7f
hppRC <hpp.ricecake@gmail.com>
2019-12-27 00:11:07 +0900

    better solution

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,18 @@
 import { WrapWithProvider } from './src/components';
 export const wrapRootElement = WrapWithProvider;
+
+import firebase from 'firebase/app';
+
+export const onClientEntry = () => {
+  const config = {
+    apiKey: 'AIzaSyBYqOHpfL1ondvkMLJvRdnrRW2tET9vl2E',
+    authDomain: 'gatsby-firebase-sample.firebaseapp.com',
+    databaseURL: 'https://gatsby-firebase-sample.firebaseio.com',
+    projectId: 'gatsby-firebase-sample',
+    storageBucket: 'gatsby-firebase-sample.appspot.com',
+    messagingSenderId: '670176804221',
+    appId: '1:670176804221:web:58f9fcd81e0c74607b2014',
+    measurementId: 'G-E49FPRK8PD'
+  };
+  firebase.initializeApp(config);
+};
